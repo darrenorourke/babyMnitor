5dd8b965be4930f6cfaf87489d03bb61669707a9
Darren O Rourke <s00094207@e1010-08.local>
2017-03-22 19:48:44 +0000

    commit

---
+++ BabyMonitor/TembooAccount.h
@@ -0,0 +1,3 @@
+#define TEMBOO_ACCOUNT "nickname"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "TZHCIZHMo4M3PWQFQA5UR47jBnziGn7d"  // your Temboo app key
